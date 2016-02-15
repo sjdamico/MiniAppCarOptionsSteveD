ab2f11e9d19e9869ede004cd6d39ff419bdd3c3c
StevesRepository <sjdamico203@sbcglobal.net>
2016-02-14 23:02:08 -0600

    Initial Commit

---
+++ MiniAppCarOptionsSteveD/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  MiniAppCarOptionsSteveD
+//
+//  Created by Steve D'Amico on 2/14/16.
+//  Copyright © 2016 Steve D'Amico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBOutlet weak var rubyRedView: UIImageView!
+    @IBOutlet weak var bronzeFireView: UIImageView!
+    @IBOutlet weak var deepImpactBlueView: UIImageView!
+    @IBOutlet weak var ingotSilverView: UIImageView!
+    @IBOutlet weak var magneticView: UIImageView!
+    @IBOutlet weak var oxfordWhiteView: UIImageView!
+    @IBOutlet weak var platinumWhiteView: UIImageView!
+    @IBOutlet weak var shadowBlackView: UIImageView!
+
+    
+    
+    @IBAction func modelButton(sender: AnyObject) {
+    }
+    @IBAction func colorsButton(sender: AnyObject) {
+    }
+    @IBAction func powerTrainButton(sender: AnyObject) {
+    }
+    @IBAction func exteriorButton(sender: AnyObject) {
+    }
+    @IBAction func interiorButton(sender: AnyObject) {
+    }
+    @IBAction func summaryButton(sender: AnyObject) {
+    }
+    
+}
+
